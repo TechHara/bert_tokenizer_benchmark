46485f60876a445c963ddf136a077f63f9e29bd9
TechHara <techhara@outlook.com>
2023-04-05 11:44:35 -0400

    add blingfire

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import argparse
+import os
 from transformers import BertTokenizerFast, BertTokenizer
 from datasets import load_dataset
-import tensorflow_hub as hub
+import blingfire
 import tensorflow_text as tf_text
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--method', type=str, choices=['hf', 'tf'])
+    parser.add_argument('--method', type=str, choices=['hf', 'tf', 'bf'])
     parser.add_argument('--input', type=str, default=None)
     parser.add_argument('--dataset', type=str, choices=['wikitext'])
 
@@ -25,7 +26,9 @@ def main():
             vocab = f.read().split()
         tokenizer = tf_text.FastBertTokenizer(vocab, lower_case_nfd_strip_accents=True, )
         tokenize = lambda s: tokenizer.tokenize([s]).numpy()[0]
-
+    elif args.method == 'bf':
+        h = blingfire.load_model(os.path.join(os.path.dirname(blingfire.__file__), "bert_base_tok.bin"))
+        tokenize = lambda s: blingfire.text_to_ids(h, s, 1575, 100, True)
     
     if args.input is not None:
         with open(args.input, 'r') as f:
