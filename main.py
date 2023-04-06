d20b04662ef6257906508f741569f63d4a239789
TechHara <techhara@outlook.com>
2023-04-06 09:43:51 -0400

    add tqdm

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from transformers import BertTokenizerFast
 from datasets import load_dataset
 import blingfire
 import tensorflow_text as tf_text
+import tqdm
 
 
 def main():
@@ -33,17 +34,20 @@ def main():
     
     if args.input is not None:
         with open(args.input, 'r') as f:
-            for line in f:
-                print(' '.join(str(idx) for idx in tokenize(line)))
-    
+            dataset = f.readlines()
+            dataset_length = len(dataset)
     else: #args.dataset is not None
         if args.dataset == 'wikitext':
             dataset = load_dataset('wikitext', 'wikitext-103-raw-v1')['train']
-        for n,text in enumerate(dataset):
-            line = text['text']
-            print(' '.join(str(idx) for idx in tokenize(line)))
-            if args.n is not None and n >= args.n:
-                break
+            dataset_length = len(dataset)
+            dataset = map(lambda text: text['text'], dataset)
+            
+
+    total = min(dataset_length, args.n)
+    for n,line in tqdm.tqdm(enumerate(dataset), total=total):
+        print(' '.join(str(idx) for idx in tokenize(line)))
+        if n >= total:
+            break
 
 
 if __name__ == '__main__':
